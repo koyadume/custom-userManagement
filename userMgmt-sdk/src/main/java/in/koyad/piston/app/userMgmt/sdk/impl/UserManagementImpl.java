270b460dfc8c76d5341d544ce743952098e8e034
Shailendra Singh <shailendra_01@outlook.com>
2015-09-12 12:12:38 +0530

    Replaced Generic REST client with Jersey Client API.

--- userMgmt-sdk/src/main/java/in/koyad/piston/app/userMgmt/sdk/impl/UserManagementImpl.java
+++ userMgmt-sdk/src/main/java/in/koyad/piston/app/userMgmt/sdk/impl/UserManagementImpl.java
@@ -15,8 +15,8 @@
  */
 package in.koyad.piston.app.userMgmt.sdk.impl;
 
-import java.net.URISyntaxException;
 import java.text.MessageFormat;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,16 +36,32 @@ public class UserManagementImpl extends AbstractREST implements UserManagementSe
 	
 	private static final LogUtil LOGGER = LogUtil.getLogger(UserManagementImpl.class);
 	
+	private static final String ROOT_RESOURCE = MessageFormat.format("http://{0}/userMgmt-service/{1}", 
+														SERVICE_HOST.concat(":").concat(String.valueOf(SERVICE_PORT)),
+														ServiceConstants.VERSION);
+	
 	@Override
 	public List<User> searchUsers(List<Attribute> atts) throws FrameworkException {
 		LOGGER.enterMethod("searchUsers");
 		
 		List<User> users = null;
 		try {
-			String resource = MessageFormat.format("/userMgmt-service/{0}/users", ServiceConstants.VERSION);
-			String query = Joiner.on('&').join(atts.stream().map(attr -> attr.getName().concat("=").concat(attr.getValue())).collect(Collectors.toList()));
+//			String resource = MessageFormat.format("/userMgmt-service/{0}/users", ServiceConstants.VERSION);
+			String query = "(".concat(
+									Joiner.on('&').join(
+														atts.stream().map(attr -> attr.getName().concat("=").concat(attr.getValue()))
+														.collect(Collectors.toList())
+													)
+									).concat(")");
+//			
+//			users = get(resource, query, List.class);
+			User[] users1 = getClient()
+								.resource(ROOT_RESOURCE)
+								.path("users")
+								.queryParam("query", query)
+								.get(User[].class);
 			
-			users = get(resource, query, List.class);
+			users = Arrays.asList(users1);
 		} catch(Exception ex) {
 			LOGGER.logException(ex);
 			throw new FrameworkException(ex.getMessage());
@@ -61,7 +77,12 @@ public class UserManagementImpl extends AbstractREST implements UserManagementSe
 
 		User user = null;
 		try {
-			user = get(MessageFormat.format("/userMgmt-service/{0}/users/{1}", ServiceConstants.VERSION, internalId), User.class);
+//			user = get(MessageFormat.format("/userMgmt-service/{0}/users/{1}", ServiceConstants.VERSION, internalId), User.class);
+			
+			user = getClient()
+						.resource(ROOT_RESOURCE)
+						.path("users").path(internalId)
+						.get(User.class);
 		} catch(Exception ex) {
 			LOGGER.logException(ex);
 			throw new FrameworkException(ex.getMessage());
@@ -77,9 +98,20 @@ public class UserManagementImpl extends AbstractREST implements UserManagementSe
 
 		try {
 			if(StringUtil.isEmpty(user.getId())) {
-				post(MessageFormat.format("/userMgmt-service/{0}/users", ServiceConstants.VERSION), user);
+//				post(MessageFormat.format("/userMgmt-service/{0}/users", ServiceConstants.VERSION), user);
+				
+				String userId = getClient()
+									.resource(ROOT_RESOURCE)
+									.path("users")
+									.post(String.class, user);
+				user.setId(userId);
 			} else {
-				put(MessageFormat.format("/userMgmt-service/{0}/users/{1}", ServiceConstants.VERSION, user.getId()), user);
+//				put(MessageFormat.format("/userMgmt-service/{0}/users/{1}", ServiceConstants.VERSION, user.getId()), user);
+				
+				getClient()
+					.resource(ROOT_RESOURCE)
+					.path("users").path(user.getId())
+					.put(String.class, user);
 			}
 		} catch(Exception ex) {
 			LOGGER.logException(ex);
@@ -94,8 +126,14 @@ public class UserManagementImpl extends AbstractREST implements UserManagementSe
 		LOGGER.enterMethod("deleteUsers");
 		
 		try {
-			String query = MessageFormat.format("userIds={0}", Joiner.on(',').join(userIds));
-			delete(MessageFormat.format("/userMgmt-service/{0}/users", ServiceConstants.VERSION), query);
+//			String query = MessageFormat.format("userIds={0}", Joiner.on(',').join(userIds));
+//			delete(MessageFormat.format("/userMgmt-service/{0}/users", ServiceConstants.VERSION), query);
+			
+			getClient()
+				.resource(ROOT_RESOURCE)
+				.path("users")
+				.queryParam("userIds", Joiner.on(',').join(userIds))
+				.delete();
 		} catch(Exception ex) {
 			LOGGER.logException(ex);
 			throw new FrameworkException(ex.getMessage());
@@ -110,10 +148,25 @@ public class UserManagementImpl extends AbstractREST implements UserManagementSe
 		
 		List<Group> groups = null;
 		try {
-			String resource = MessageFormat.format("/userMgmt-service/{0}/groups", ServiceConstants.VERSION);
-			String query = Joiner.on('&').join(atts.stream().map(attr -> attr.getName().concat("=").concat(attr.getValue())).collect(Collectors.toList()));
+//			String resource = MessageFormat.format("/userMgmt-service/{0}/groups", ServiceConstants.VERSION);
+//			String query = Joiner.on('&').join(atts.stream().map(attr -> attr.getName().concat("=").concat(attr.getValue())).collect(Collectors.toList()));
+//			
+//			groups = get(resource, query, List.class);
 			
-			groups = get(resource, query, List.class);
+			String query = "(".concat(
+									Joiner.on('&').join(
+													atts.stream().map(attr -> attr.getName().concat("=").concat(attr.getValue()))
+													.collect(Collectors.toList())
+												)
+								).concat(")");
+			
+			Group[] groups1 = getClient()
+								.resource(ROOT_RESOURCE)
+								.path("groups")
+								.queryParam("query", query)
+								.get(Group[].class);
+
+			groups = Arrays.asList(groups1);
 		} catch(Exception ex) {
 			LOGGER.logException(ex);
 			throw new FrameworkException(ex.getMessage());
@@ -129,7 +182,12 @@ public class UserManagementImpl extends AbstractREST implements UserManagementSe
 
 		Group group = null;
 		try {
-			group = get(MessageFormat.format("/userMgmt-service/{0}/groups/{1}", ServiceConstants.VERSION, internalId), Group.class);
+//			group = get(MessageFormat.format("/userMgmt-service/{0}/groups/{1}", ServiceConstants.VERSION, internalId), Group.class);
+			
+			group = getClient()
+					.resource(ROOT_RESOURCE)
+					.path("groups").path(internalId)
+					.get(Group.class);
 		} catch(Exception ex) {
 			LOGGER.logException(ex);
 			throw new FrameworkException(ex.getMessage());
@@ -145,9 +203,20 @@ public class UserManagementImpl extends AbstractREST implements UserManagementSe
 		
 		try {
 			if(StringUtil.isEmpty(group.getId())) {
-				post(MessageFormat.format("/userMgmt-service/{0}/groups", ServiceConstants.VERSION), group);
+//				post(MessageFormat.format("/userMgmt-service/{0}/groups", ServiceConstants.VERSION), group);
+				
+				String groupId = getClient()
+									.resource(ROOT_RESOURCE)
+									.path("groups")
+									.post(String.class, group);
+				group.setId(groupId);
 			} else {
-				put(MessageFormat.format("/userMgmt-service/{0}/groups/{1}", ServiceConstants.VERSION, group.getId()), group);
+//				put(MessageFormat.format("/userMgmt-service/{0}/groups/{1}", ServiceConstants.VERSION, group.getId()), group);
+				
+				getClient()
+					.resource(ROOT_RESOURCE)
+					.path("groups").path(group.getId())
+					.put(String.class, group);
 			}
 		} catch(Exception ex) {
 			LOGGER.logException(ex);
@@ -162,8 +231,14 @@ public class UserManagementImpl extends AbstractREST implements UserManagementSe
 		LOGGER.enterMethod("deleteGroups");
 		
 		try {
-			String query = MessageFormat.format("groupIds={0}", Joiner.on(',').join(groupIds));
-			delete(MessageFormat.format("/userMgmt-service/{0}/groups", ServiceConstants.VERSION), query);
+//			String query = MessageFormat.format("groupIds={0}", Joiner.on(',').join(groupIds));
+//			delete(MessageFormat.format("/userMgmt-service/{0}/groups", ServiceConstants.VERSION), query);
+			
+			getClient()
+				.resource(ROOT_RESOURCE)
+				.path("groups")
+				.queryParam("groupIds", Joiner.on(',').join(groupIds))
+				.delete();
 		} catch(Exception ex) {
 			LOGGER.logException(ex);
 			throw new FrameworkException(ex.getMessage());
