ca9e97b4aec08928df65c8f9062d83cb715637a7
Shailendra Singh <shailendra_01@outlook.com>
2015-09-14 16:37:01 +0530

    Minor updates.

--- userMgmt-service/src/main/java/in/koyad/piston/app/userMgmt/service/resources/UserResource.java
+++ userMgmt-service/src/main/java/in/koyad/piston/app/userMgmt/service/resources/UserResource.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2012-2015 Shailendra Singh <shailendra_01@outlook.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package in.koyad.piston.app.userMgmt.service.resources;
 
 import java.util.ArrayList;
@@ -24,6 +39,12 @@ import in.koyad.piston.common.exceptions.FrameworkException;
 import in.koyad.piston.common.utils.LogUtil;
 import in.koyad.piston.common.utils.StringUtil;
 
+/**
+ * 
+ *
+ * @author Shailendra Singh
+ * @since 1.0
+ */
 @Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
